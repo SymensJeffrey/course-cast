b2c9bc6ac1cb6af2c00ddba078efc1030d8bf3f4
symensjeffrey <jeffrey.symens23@gmail.com>
2026-01-24 22:42:14 -0600

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-3xl font-bold">🏌️ Golf Scoreboard</h1>
+      <div>
+        <Button>Click Me</Button>
+      </div>
     </main>
   );
 }
