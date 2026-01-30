17c9fa23103b7998b0aa98833007f3db6064b060
symensjeffrey <jeffrey.symens23@gmail.com>
2026-01-29 21:29:05 -0600

    add enter team name

--- src/app/admin/[code]/page.tsx
+++ src/app/admin/[code]/page.tsx
@@ -1,4 +1,7 @@
 import { CopyButton } from '@/components/CopyButton';
+import { Button } from "@/components/ui/button"
+import { Field } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
 
 export default async function AdminPage({
   params,
@@ -8,22 +11,26 @@ export default async function AdminPage({
   const { code } = await params;
 
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center gap-6">
+    <main className="min-h-screen flex flex-col  justify-center gap-6">
       <div className="p-6 space-y-4">
-        <h1 className="text-2xl font-bold">Share this code</h1>
-
-        <div className="flex items-center justify-center gap-3">
-          <div>
+        <div className='justify-cente items-center'>
+          <h1 className="text-2xl font-bold">Share this code</h1>
+        </div>
+        <div className="flex flex-col items-center justify-center gap-3">
+          <div className="flex flex-row items-center gap-3">
             <p className="text-3xl font-mono tracking-widest">
               {code}
             </p>
-          </div>
-          <div>
             <CopyButton value={code} />
           </div>
-
+          <div>
+            <Field orientation="horizontal">
+              <Input placeholder="Enter Team Name" />
+              <Button>Submit</Button>
+            </Field>
+          </div>
         </div>
-      </div>
+      </div >
     </main >
   );
 }
