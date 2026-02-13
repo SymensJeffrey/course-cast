f7c547a1627680e57d9ffabf8cdcfdc86121961b
symensjeffrey <jeffrey.symens23@gmail.com>
2026-02-13 12:55:53 -0600

    update padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -165,7 +165,7 @@ export default function Home() {
   }
 
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center gap-6">
+    <main className="pb-36 min-h-screen flex flex-col items-center justify-center gap-6">
       <h1 className="text-3xl font-bold">Welcome to Course Cast</h1>
 
       <div className="flex gap-4">
